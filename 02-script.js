e6c393369e3fe32ee7155d9c5226affdae43d0a3
marysant09 <steffani_santos@estudante.sesisenai.org.br>
2025-06-18 15:54:14 -0300

    atividade 5.4

---
+++ 02-script.js
@@ -0,0 +1,13 @@
+//5.5a
+let nota1 = Number(prompt("Digite a a primeira nota:"))
+let nota2 = Number(prompt("Digite a segunda nota"))
+
+let media = (nota1 + nota2) / 2
+if(media >= 7){
+
+    alert("Vc passou " + media)
+}
+else{
+
+    alert("Vc n passou " + media)
+}

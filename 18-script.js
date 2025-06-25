685ad709b2d661fc39858532cff8d167d73c6239
marysant09 <steffani_santos@estudante.sesisenai.org.br>
2025-06-25 16:53:09 -0300

    Add files via upload

---
+++ 18-script.js
@@ -0,0 +1,6 @@
+//5.18
+let distancia = Number(prompt("Digite a distancia em km:"));
+let velocidade = 300000
+let tempo = distancia/ velocidade;
+
+alert(" a viagem vai durar " + tempo + " segundos");

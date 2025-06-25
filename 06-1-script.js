647fabd250413fb86074bac7827e3696d6777905
marysant09 <steffani_santos@estudante.sesisenai.org.br>
2025-06-25 16:54:06 -0300

    Add files via upload

---
+++ 06-1-script.js
@@ -0,0 +1,15 @@
+//5.6
+let pontos =Number(prompt(" Digite sua pontuação:"));
+
+if(nota <= 10){
+    alert("deu ruim")
+}
+if(nota >10 || nota<=100){
+    alert("tá quase querido, mais um pouco e vc consegue")
+}
+if(nota >100 || nota <=200){
+    alert("vc esta divando")
+}
+if(nota >200){
+    alert("arrasou, vc é um iconico")
+}
